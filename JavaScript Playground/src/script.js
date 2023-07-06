a41e0411ccff042106bc1b7bd65602d104095af6
naho0201 <138743333+naho0201@users.noreply.github.com>
2023-07-06 15:07:12 +0900

    Add files via upload

---
+++ JavaScript Playground/src/script.js
@@ -0,0 +1,24 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+
+
+//課題１
+console.log(message)
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+//課題２
+let food = ["うどん","ラーメン","冷やし中華"]
+ 
+//課題３
+
+for(let i = 0; i< food.length; i++){
+ console.log(food[i]); 
+
+}
+
+//課題４
